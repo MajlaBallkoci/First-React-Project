e5d0f85103d4a74e91f57370620205f29e1e7a80
MajlaBallkoci <you@example.com>
2022-09-15 15:42:40 +0200

    first commit

---
+++ src/component/NewComponentExpenses.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./NewComponentExpenses.css";
+import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+function NewComponentExpenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
+
+export default NewComponentExpenses;
